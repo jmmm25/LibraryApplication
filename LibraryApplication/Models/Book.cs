39ba2fea9f57477e9e149cc6e1092fa38c2d4913
김정민 <jmkim9302@gmail.com>
2022-05-29 22:57:18 +0900

    Add EntityFramework and Library DB

---
+++ LibraryApplication/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryApplication.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Book_U { get; set; }
+        public string Title { get; set; }
+        public string Writer { get; set; }
+        public string Summary { get; set; }
+        public string Publisher { get; set; }
+        public int Published_date { get; set; }
+    }
+}
